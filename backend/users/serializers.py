3d9fb62d30dff1d8997b58c9e0349e7a8791471e
prositedeveloper <aga.davdav@yandex.ru>
2026-06-03 12:24:03 +0300

    Fix serializers users

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User 
-        fields = ['id', 'username', 'email', 'firstname', 'last_name', 'phone', 'avatar']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'phone', 'avatar']
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
